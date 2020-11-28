86dfb86592bb27b56e402e60c08499f56a8fab90
Isaque Melo <isaquegbmelo2@gmail.com>
2020-11-28 14:59:55 -0300

    Add basic docs

--- src/Validator.php
+++ src/Validator.php
@@ -7,14 +7,45 @@ class Validator{
         BankCodeMapping::init();
     }
 
+    /**
+     * validate_account
+     *
+     * @param [string] $bank_code
+     * @param [string] $account
+     * @param [string] $digit
+     * @return boolean
+     */
     public static function validate_account($bank_code, $account, $digit) {
         $validator = BankCodeMapping::get_validator($bank_code);
         return $validator::validate_account_digit($account, $digit);
     }
 
+    /**
+     * validate_agency
+     *
+     * @param [string] $bank_code
+     * @param [string] $agency
+     * @param [string] $digit
+     * @return boolean
+     */
     public static function validate_agency($bank_code, $agency, $digit) {
         $validator = BankCodeMapping::get_validator($bank_code);
         return $validator::validate_agency_digit($agency, $digit);
+    }
+    
+    /**
+     * validate_bank_account
+     *
+     * @param [string] $bank_code
+     * @param [string] $account
+     * @param [string] $account_digit
+     * @param [string] $agency
+     * @param [string] $agency_digit
+     * @return boolean
+     */
+    public static function validate_bank_account($bank_code, $account, $account_digit, $agency, $agency_digit) {
+        $validator = BankCodeMapping::get_validator($bank_code);
+        return $validator::validate_account_digit($account, $account_digit) && $validator::validate_agency_digit($agency, $agency_digit);
     }
 
 }
--- src/interfaces/ValidatorI.php
+++ src/interfaces/ValidatorI.php
@@ -1,6 +1,9 @@
 <?php 
 namespace BankValidator\interfaces;
 
+/**
+ * Inteface used that all banks implement
+ */
 interface ValidatorI {
     public static function validate_agency_digit($agency, $digit);
     public static function validate_account_digit($account, $digit);
